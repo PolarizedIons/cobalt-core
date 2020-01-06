932d59b209eb1bae1c6ddbb500d992892e937150
PolarizedIons <polarizedions@gmail.com>
2020-01-06 20:58:19 +0200

    !echo

--- src/reactor/index.js
+++ src/reactor/index.js
@@ -1,5 +1,12 @@
 module.exports = nats => {
     nats.subscribe("message", msg => {
         console.log("[MESSAGE]", msg);
+
+        if (msg.message.startsWith("!echo")) {
+            nats.publish("reply", {
+                channel: msg.channel,
+                message: msg.message.substring(5),
+            });
+        }
     });
 };
